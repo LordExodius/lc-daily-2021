d7b946f1ec5d5c054e116505f819f1bb38f009e6
LordExodius <oscar45697@gmail.com>
2021-05-21 20:47:16 -0400

    optimized smallest string

--- Smallest String With A Given Numeric Value/smallString.py
+++ Smallest String With A Given Numeric Value/smallString.py
@@ -25,6 +25,28 @@ def getSmallestString2(self, n: int, k: int) -> str:
         string = f"{currentLetter + 96:c}" + string
     return f"{string:a>{n}}"
 
+def getSmallestString3(self, n: int, k: int) -> str:
+    k -= n
+    string = chr((k + k//25)%26 + 97) + k//25 * "z"
+    while len(string) > n:
+        string = string[1:]
+    return f"{string:a>{n}}"
 
-print(getSmallestString(0, 5, 73))
-print(getSmallestString2(0, 5, 73))
+print("\nTEST 1")
+print("Solution2:",getSmallestString2(0, 5, 73))
+print("Solution3:",getSmallestString3(0, 5, 73))
+print("\nTEST 2")
+print("Solution2:",getSmallestString2(0, 3, 27))
+print("Solution3:",getSmallestString3(0, 3, 27))
+print("\nTEST 3")
+print("Solution2:",getSmallestString2(0, 5, 130))
+print("Solution3:",getSmallestString3(0, 5, 130))
+print("\nTEST 4")
+print("Solution2:",getSmallestString2(0, 24, 552))
+print("Solution3:",getSmallestString3(0, 24, 552))
+print("\nTEST 5")
+print("Solution2:",getSmallestString2(0, 90, 1191))
+print("Solution3:",getSmallestString3(0, 90, 1191))
+
+"aadzzzzzzzzzzzzzzzzzzzzz"
+"aaaczzzzzzzzzzzzzzzzzzzz"
